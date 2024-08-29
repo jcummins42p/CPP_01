9a5e565c2133253ef4d9d902902c9da7f4e90267
jcummins <jcummins42prague@gmail.com>
2024-08-29 18:23:57 +0200

    Done ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,11 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcummins <jcummins@student.42prague.com>   +#+  +:+       +#+        */
+/*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/11 13:40:44 by jcummins          #+#    #+#             */
-/*   Updated: 2024/08/11 13:41:18 by jcummins         ###   ########.fr       */
+/*   Created: 2024/08/29 18:12:59 by jcummins          #+#    #+#             */
+/*   Updated: 2024/08/29 18:20:59 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cpp_00.h"
+#include <iostream>
+
+int	main(void)
+{
+	std::string	brain = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &brain;
+	std::string &stringREF = brain;
+
+	std::cout << &brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << brain << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
